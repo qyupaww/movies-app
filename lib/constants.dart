24b9fcff7ef52b0314b5bae636b9eb6a72cc682c
Faisal Rahman <qyupaww@gmail.com>
2025-11-20 14:18:15 +0700

    feat: color pallete

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete {
+  static const primaryColor = Color(0xFFE50914);
+  static const secondaryColor = Color(0xFFB20710);
+  static const mineShaft = Color(0xFF221F1F);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFF5F5F1);
+  static const grey = Color(0xFF564D4D);
+  static const darkGrey = Color(0xFF393434);
+}
