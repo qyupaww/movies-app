70b1f854e3745e3f7e70844e1f110188f3307315
Faisal Rahman <qyupaww@gmail.com>
2025-11-21 20:59:43 +0700

    UI: vertical movie grid widget

---
+++ lib/widgets/vertical_movie_grid_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/constants.dart';
+import 'package:movies_app/widgets/rounded_image_widget.dart';
+import 'package:movies_app/widgets/title_list_widget.dart';
+
+class VerticalMovieGridWidget extends StatelessWidget {
+  final String title;
+  const VerticalMovieGridWidget({
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TitleListWidget(
+          padding: const EdgeInsets.only(left: 24),
+          title: title,
+        ),
+        const SizedBox(height: 8),
+        GridView.builder(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+          ),
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 6,
+                childAspectRatio: 82 / 96,
+                crossAxisSpacing: 6,
+              ),
+          itemBuilder: (context, index) {
+            return RoundedImageWidget(
+              path:
+                  "assets/images/${DummyData.backdropListsPath[index]}",
+            );
+          },
+          itemCount: DummyData.backdropListsPath.length,
+        ),
+      ],
+    );
+  }
+}
