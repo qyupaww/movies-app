7e0d4f56e9f87bd878a4ccd1f79421db0c7524fa
Faisal Rahman <qyupaww@gmail.com>
2025-11-21 20:48:42 +0700

    UI: vertical movie list widget

---
+++ lib/widgets/vertical_movie_list_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/widgets/movie_tile_widget.dart';
+import 'package:movies_app/widgets/title_list_widget.dart';
+
+import '../constants.dart';
+
+class VerticalMovieListWidget extends StatelessWidget {
+  final String title;
+  const VerticalMovieListWidget({
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TitleListWidget(
+          padding: const EdgeInsets.only(left: 24),
+          title: title,
+        ),
+        const SizedBox(height: 8),
+        ListView.separated(
+          itemBuilder: (context, index) {
+            return MovieTileWidget(
+              title: DummyData.backdropListsTitle[index],
+              assetPath:
+                  "assets/images/${DummyData.backdropListsPath[index]}",
+            );
+          },
+          separatorBuilder: (_, __) {
+            return const SizedBox(height: 8);
+          },
+          itemCount: DummyData.backdropListsPath.length,
+        ),
+      ],
+    );
+  }
+}
