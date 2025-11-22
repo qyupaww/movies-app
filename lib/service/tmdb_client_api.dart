3bb72e1a19ee96e1cbef724784ae2227d6eaaf4f
Faisal Rahman <qyupaww@gmail.com>
2025-11-22 20:03:56 +0700

    feat:tmdb client api

--- lib/service/tmdb_client_api.dart
+++ lib/service/tmdb_client_api.dart
@@ -4,7 +4,7 @@ import 'package:retrofit/retrofit.dart';
 import '../model/movie_details.dart';
 import '../model/movie_response_data.dart';
 
-part 'tmdb_clien_api.g.dart';
+part 'tmdb_client_api.g.dart';
 
 @RestApi(baseUrl: 'http://api.themoviedb.org/3')
 abstract class TmdbClientApi {
---
+++ lib/service/tmdb_client_api.g.dart
@@ -0,0 +1,251 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tmdb_client_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _TmdbClientApi implements TmdbClientApi {
+  _TmdbClientApi(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'http://api.themoviedb.org/3';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<MovieResponseData> getTrendingMovieBy(String time) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieResponseData>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/trending/movie/${time}?language=en-US',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieResponseData.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<MovieResponseData> getNowPlayingMovies({
+    required int page,
+    required String language,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'page': page,
+      r'language': language,
+    };
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieResponseData>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/movie/now_playing',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieResponseData.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<MovieResponseData> getMoviesByGenre({
+    required bool includeAdult,
+    required bool includeVideo,
+    required int page,
+    required String sortBy,
+    required int withGenres,
+    required int language,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'include_adult': includeAdult,
+      r'include_video': includeVideo,
+      r'page': page,
+      r'sort_by': sortBy,
+      r'with_genres': withGenres,
+      r'language': language,
+    };
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieResponseData>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/discover/movie',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieResponseData.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<MovieResponseData> getMoviesByQuery({
+    required String query,
+    required bool includeAdult,
+    required int language,
+    required int page,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'query': query,
+      r'include_adult': includeAdult,
+      r'language': language,
+      r'page': page,
+    };
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieResponseData>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/search.movie',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieResponseData.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<MovieDetails> getMovieDetailsById({required int movieId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieDetails>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'movie/${movieId}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieDetails.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<MovieDetails> getSimilarMovieDetailsById({
+    required int movieId,
+    required int language,
+    required int page,
+  }) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'language': language,
+      r'page': page,
+    };
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<MovieDetails>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'movie/${movieId}/similar',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = MovieDetails.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
