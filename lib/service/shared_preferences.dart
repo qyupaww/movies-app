b8fcc63111a25d11c53c414c75cb5b4d9c236fe3
Faisal Rahman <qyupaww@gmail.com>
2025-11-22 20:46:36 +0700

    feat: shared preferences

---
+++ lib/service/shared_preferences.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../model/movie_general.dart';
+
+class MoviesSharedPreferences {
+  static const myMoviesListKey = "my_movies_list_key";
+
+  MoviesSharedPreferences() {
+    _initSharedPreferences();
+  }
+
+  SharedPreferences? _sharedPreferences;
+
+  Future<void> _initSharedPreferences() async {
+    if (_sharedPreferences != null) return;
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future<List<MovieGeneral>> getAllMovies() async {
+    await _initSharedPreferences();
+    if (_sharedPreferences != null) {
+      final moviesJson = _sharedPreferences!.getString(myMoviesListKey)
+    }
+  }
+}
