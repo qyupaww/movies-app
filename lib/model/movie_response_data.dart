8362269bbfee9004a651efafe4857f88a7c89d7e
Faisal Rahman <qyupaww@gmail.com>
2025-11-22 18:42:41 +0700

    feat: model movie n genre

---
+++ lib/model/movie_response_data.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'movie_general.dart';
+part 'movie_response_data.g.dart';
+
+@JsonSerializable(fieldRename: .snake)
+class MovieResponseData {
+  final int page;
+  final List<MovieGeneral> results;
+  final int totalPages;
+  final int totalResults;
+
+  const MovieResponseData({
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  factory MovieResponseData.fromJson(
+    Map<String, dynamic> json,
+  ) => _$MovieResponseDataFromJson(json);
+
+  Map<String, dynamic> toJson() =>
+      _$MovieResponseDataToJson(this);
+}
