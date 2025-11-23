fad0c587e5146fb3cf5ec8e3e32f8db27689f860
Faisal Rahman <qyupaww@gmail.com>
2025-11-23 16:25:42 +0700

    feat: integrasi data UI

--- lib/views/search_screen.dart
+++ lib/views/search_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/bloc/search_bloc/search_bloc.dart';
 import 'package:movies_app/constants.dart';
 import 'package:movies_app/widgets/search_bar_widget.dart';
 import 'package:movies_app/widgets/vertical_movie_list_widget.dart';
+
+import '../widgets/vertical_movie_grid_widget.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -13,9 +17,28 @@ class SearchScreen extends StatelessWidget {
         backgroundColor: ColorPallete.black,
         body: ListView(
           children: [
-            SearchBarWidget(),
-            SizedBox(height: 12),
-            VerticalMovieListWidget(title: "Now Playing"),
+            const SearchBarWidget(),
+            const SizedBox(height: 12),
+            BlocBuilder<SearchBloc, SearchState>(
+              buildWhen: (_, current) =>
+                  current.status == SearchStatus.loaded ||
+                  current.status == SearchStatus.loadedSearchResult,
+              builder: (context, state) {
+                if (state.status == SearchStatus.loaded) {
+                  return VerticalMovieListWidget(
+                    title: "Now Playing",
+                    movies: state.movies,
+                  );
+                } else if (state.status == SearchStatus.loadedSearchResult) {
+                  return VerticalMovieGridWidget(
+                    title: "Results",
+                    movies: state.movies,
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            )
           ],
         ),
       ),
