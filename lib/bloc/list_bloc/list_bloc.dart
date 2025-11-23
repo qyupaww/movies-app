6a02a06ec71b39ddd4886a2c72656630d3b52cc0
Faisal Rahman <qyupaww@gmail.com>
2025-11-23 17:13:18 +0700

    feat: list bloc - event,state

---
+++ lib/bloc/list_bloc/list_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../model/movie_general.dart';
+import '../../service/shared_preferences.dart';
+
+part 'list_event.dart';
+part 'list_state.dart';
+
+class MyListBloc extends Bloc<MyListEvent, MyListState> {
+  final MoviesSharedPreferences _sharedPreferences;
+
+  MyListBloc(
+    this._sharedPreferences,
+  ) : super(MyListState()) {
+    on<MyListInitEvent>(_onInitMyList);
+  }
+
+  void _onInitMyList(
+    MyListInitEvent event,
+    Emitter<MyListState> emit,
+  ) async {
+    final movies = await _sharedPreferences.getAllMovies();
+
+    emit(MyListState(
+      status: MyListStatus.loaded,
+      movies: movies,
+    ));
+  }
+}
