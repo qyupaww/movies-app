21806f0f88b8aee4efd9c7f1d3c8851c91461dec
Faisal Rahman <qyupaww@gmail.com>
2025-11-23 14:28:38 +0700

    feat: implement Bloc State Management

--- lib/bloc/search_bloc/search_bloc.dart
+++ lib/bloc/search_bloc/search_bloc.dart
@@ -1,4 +1,55 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../model/movie_general.dart';
+import '../../model/movie_response_data.dart';
+import '../../service/tmdb_client_api.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
+
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  final TmdbClientApi _tmdbClientApi;
+
+  SearchBloc(
+    this._tmdbClientApi,
+  ) : super(SearchState()) {
+    on<SearchInitEvent>(_onInitSearch);
+    on<SearchQueryEvent>(_onQuerySearch);
+  }
+
+  void _onInitSearch(
+    SearchInitEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    MovieResponseData data = await _tmdbClientApi.getNowPlayingMovies(
+      page: 1,
+      language: 'en-US',
+    );
+
+    List<MovieGeneral> movies = data.results;
+
+    emit(SearchState(
+      status: SearchStatus.loaded,
+      movies: movies,
+    ));
+  }
+
+  void _onQuerySearch(
+    SearchQueryEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    MovieResponseData data = await _tmdbClientApi.getMoviesByQuery(
+      query: event.query,
+      includeAdult: true,
+      language: 'en-US',
+      page: 1,
+    );
+
+    List<MovieGeneral> movies = data.results;
+
+    emit(SearchState(
+      status: SearchStatus.loadedSearchResult,
+      movies: movies,
+    ));
+  }
+}
