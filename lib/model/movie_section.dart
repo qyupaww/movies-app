c793be82b317269266e027390e34256101622c50
Faisal Rahman <qyupaww@gmail.com>
2025-11-23 10:22:05 +0700

    feat:movie section

---
+++ lib/model/movie_section.dart
@@ -0,0 +1,13 @@
+import 'movie_general.dart';
+
+class MovieSection {
+  final String title;
+  final HomeSectionType sectionType;
+  final List<MovieGeneral> movies;
+
+  const MovieSection({
+    required this.title,
+    required this.sectionType,
+    required this.movies,
+  });
+}
