d1578928308dc8a11fd9aad1a466a935a994da57
Faisal Rahman <qyupaww@gmail.com>
2025-11-23 17:07:45 +0700

    feat: list bloc - event,state

---
+++ lib/bloc/list_bloc/list_state.dart
@@ -0,0 +1,26 @@
+part of "list_bloc.dart";
+
+enum MyListStatus {
+  initial,
+  loaded,
+}
+
+class MyListState {
+  final MyListStatus status;
+  final List<MovieGeneral> movies;
+
+  const MyListState({
+    this.status = MyListStatus.initial,
+    this.movies = const <MovieGeneral>[],
+  });
+
+  MyListState copyWith({
+    MyListStatus? status,
+    List<MovieGeneral>? movies,
+  }) {
+    return MyListState(
+      status: status ?? this.status,
+      movies: movies ?? this.movies,
+    );
+  }
+}
